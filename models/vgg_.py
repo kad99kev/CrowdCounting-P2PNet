0975bd589a85fad7994cf99d2305a3731893741d
Kevlyn Kadamala <kevlyn@gmail.com>
2022-12-13 23:43:54 +0000

    Test with urls

--- models/vgg_.py
+++ models/vgg_.py
@@ -103,7 +103,7 @@ def _vgg(arch, cfg, batch_norm, pretrained, progress, sync=False, **kwargs):
         kwargs['init_weights'] = False
     model = VGG(make_layers(cfgs[cfg], batch_norm=batch_norm, sync=sync), **kwargs)
     if pretrained:
-        state_dict = torch.load(model_paths[arch])
+        state_dict = torch.load(model_urls[arch])
         model.load_state_dict(state_dict)
     return model
 
